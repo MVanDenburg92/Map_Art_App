668e74ca721e95dc897405c3bd41e5efc64511f7
Miles Van Denburg <miles.vandenburg@gmail.com>
2025-04-29 15:01:31 -0400

    misc

--- map_art_app/app.py
+++ map_art_app/app.py
@@ -277,9 +277,8 @@ def main():
                     st.rerun()
         with st.expander("Need help? Click to view tutorial"):
             st.markdown("""There are two ways to move the marker: 
-                         - 1. Click a spot on the map and then click the 'Move the Marker' button; or 
-                         - 2. Double-Click twice on a spot on the map and the marker will move. 
-                        
+                        -- 1. Click a spot on the map and then click the 'Move the Marker' button; or 
+                        -- 2. Double-Click twice on a spot on the map and the marker will move. 
                         Once you have your marker on the right spot feel free to customize your map using the options below.  
                         There is no limit on how many maps you can make, so experiment and play around!""")
         bbox_size = st.slider("Bounding Box Size", 0.005, 0.05, 0.015, step=0.005)
